4cbf3d3632d48fa1d3f80186c6f2dce40b85dd05
Anwar Ziani <anwaar.ziani@gmail.com>
2020-01-19 15:03:00 -0800

    Add categories

--- src/components/CategoriesContainer.vue
+++ src/components/CategoriesContainer.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="d-flex flex-wrap">
-    Categories Container
+  <div class="p-5 d-flex flex-wrap">
+    <h2>Categories</h2>
+    <div class="d-flex w-100 mt-5">
+      <div class="card p-4 mr-2">card 1</div>
+      <div class="card p-4 mr-2">card 2</div>
+      <div class="card p-4 mr-2">card 3</div>
+    </div>
   </div>
 </template>
 
